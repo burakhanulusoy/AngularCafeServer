d608a55524181e107af8277c8d5491a4e5ebbc20
Burak Han Ulusoy <burakhanulusoy18@gmail.com>
2026-02-10 22:31:21 +0300

    Add project files.

---
+++ AngularCafeServer/DTOs/MenuDtos/CreateMenuDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AngularCafeServer.DTOs.MenuDtos
+{
+    public class CreateMenuDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ AngularCafeServer/Entities/Menu.cs
@@ -0,0 +1,14 @@
+﻿namespace AngularCafeServer.Entities
+{
+    public class Menu
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+
+        public Category Category { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
